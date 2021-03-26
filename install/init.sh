98590de683510b4f6502b08d6419f26b41ef95e6
dnzmfr <7255778+DnZmfr@users.noreply.github.com>
2021-03-26 23:19:52 +0000

    Update init.sh

--- install/init.sh
+++ install/init.sh
@@ -6,7 +6,47 @@ bold() {
 
 export EKS_CLUSTER_NAME=spinnaker-eks
 export NAMESPACE=spinnaker
-export GITHUB_ACCOUNT_NAME=dnzmfr
-export GITHUB_TOKEN_FILE=/home/ec2-user/.github.tkn
-export DOCKER_ACCOUNT_NAME=dnzmfr
-export DOCKER_PASS_FILE=/home/ec2-user/.docker.psw
+
+# github credentials
+if [ ! -f ~/.github.tkn ]; then
+  echo "github.tkn file is missing."
+  exit 1
+elif [ -s ~/.github.tkn ]; then
+  echo "github.tkn file is empty. token missing."
+  exit 1
+else
+  export GITHUB_ACCOUNT_NAME=dnzmfr
+  export GITHUB_TOKEN_FILE=~/.github.tkn
+fi
+
+# docker credentials
+if [ ! -f ~/.docker.psw ]; then
+  echo "docker.psw file is missing."
+  exit 1
+elif [ -s ~/.docker.psw ]; then
+  echo "docker.psw file is empty. password missing."
+  exit 1
+else
+  export DOCKER_ACCOUNT_NAME=dnzmfr
+  export DOCKER_PASS_FILE=~/.docker.psw
+fi
+
+# aws credentials
+if [ ! -f ~/.aws/credentials ]; then
+  echo "aws credentials file is missing."
+elif [ -s ~/.aws/credentials ]; then
+  echo "aws credentials file is empty."
+else
+  export AWS_ID_ACCESS_KEY=$(grep aws_access_key_id ~/.aws/credentials| awk '{print $NF}')
+  export AWS_SECRET_ACCESS_KEY=$(grep aws_secret_access_key ~/.aws/credentials| awk '{print $NF}')
+fi
+
+# minio credentials
+if [ ! -f ~/.minio.creds ]; then
+  echo "docke file is missing."
+elif [ -s ~/.minio.creds ]
+  echo "minio.creds file is empty. credentials missing."
+else
+  export MINIO_ACCESS_KEY=$(grep ACCESS_KEY ~/.minio.creds| awk -F"=" '{print $2}')
+  export MINIO_SECRET_KEY=$(grep SECRET_KEY ~/.minio.creds| awk -F"=" '{print $2}')
+fi
