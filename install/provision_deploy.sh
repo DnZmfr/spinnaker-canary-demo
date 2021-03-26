5d459d31aa13b4f942f86095cbb3869139df4dbe
dnzmfr <7255778+DnZmfr@users.noreply.github.com>
2021-03-26 14:36:03 +0000

    Update provision script and trigger-deploy pipeline

--- install/provision_deploy.sh
+++ install/provision_deploy.sh
@@ -2,8 +2,14 @@
 
 source init.sh
 
+if [ $USER != "root" ]; then
+  echo "This script should be executed as root."
+  bold "Run: sudo $0"
+  exit 1
+fi
+
 if [ ! -f /usr/local/bin/kubectl ]; then
-  bold "Installing kubectl cli..."
+  bold "Install kubectl cli..."
   curl -o /usr/local/bin/kubectl -sLO https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
   chmod +x /usr/local/bin/kubectl
 else
@@ -11,19 +17,20 @@ else
 fi
 
 if [ ! -f /usr/local/bin/aws-iam-authenticator ]; then
-  bold "Installing aws-iam-authenticator cli..."
+  bold "Install aws-iam-authenticator cli..."
   curl -o /usr/local/bin/aws-iam-authenticator -sLO https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator
   chmod +x /usr/local/bin/aws-iam-authenticator
 else
   bold "aws-iam-authenticator cli already installed."
 fi
 
-bold "Installing awscli..."
+bold "Install awscli..."
+yum install -y python3-pip
 pip3 install awscli --upgrade --user
 
 if [ ! -f /usr/local/bin/eksctl ]; then
-  bold "Installing eksctl cli..."
-  curl -sLO "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+  bold "Install eksctl cli..."
+  curl -sL "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
   mv /tmp/eksctl /usr/local/bin/
   chmod +x /usr/local/bin/eksctl
   rm -f eksctl_$(uname -s)_amd64.tar.gz
@@ -31,16 +38,20 @@ else
   bold "eksctl cli already installed."
 fi
 
+bold "Install java..."
+yum install -y java-11-amazon-corretto.x86_64
+
 if [ ! -f /usr/local/bin/hal ]; then
-  bold "Installing Halyard..."
+  bold "Install Halyard..."
   curl -sO https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh
-  bash InstallHalyard.sh -y --user ci_cd
+  bash InstallHalyard.sh -y --user ec2-user
+  rm -f InstallHalyard.sh
 else
   bold "Halyard cli already installed."
 fi
 
 if [ ! -f /usr/local/bin/spin ]; then
-  bold "Installing spin cli..."
+  bold "Install spin cli..."
   curl -o /usr/local/bin/spin -sLO https://storage.googleapis.com/spinnaker-artifacts/spin/$(curl -s https://storage.googleapis.com/spinnaker-artifacts/spin/latest)/linux/amd64/spin
   chmod +x /usr/local/bin/spin
 else
@@ -48,11 +59,14 @@ else
 fi
 
 if [ ! -f /usr/bin/docker ]; then
-  bold "Installing docker..."
+  bold "Install docker..."
   yum install -y docker
 else
   bold "docker  already installed."
 fi
+
+bold "Install git"
+yum install -y git
 
 ### Setup github runner
 #mkdir actions-runner && cd actions-runner
