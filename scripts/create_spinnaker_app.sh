49d225d84e6ef34f7dfb4373191fcd4ca7611f93
dnzmfr <7255778+DnZmfr@users.noreply.github.com>
2021-03-27 17:38:50 +0000

    Update script

--- scripts/create_spinnaker_app.sh
+++ scripts/create_spinnaker_app.sh
@@ -3,11 +3,11 @@
 source init.sh
 
 bold "Create a spinnaker demo app"
-spin application save --file ../spinnaker/app.json
+spin application save --file ../spinnaker/app-demo.json
 sleep 5
 
 bold "Create canary config"
-spin canary canary-config save -f ../spinnaker/canary-config.json
+spin canary canary-config save -f ../spinnaker/config-canary.json
 
 bold "Create trigger deploy pipeline"
 spin pipeline save --file ../spinnaker/pipeline-trigger-deploy.json
@@ -22,10 +22,9 @@ spin pipeline save --file ../spinnaker/pipeline-canary-deploy.json
 # the actual ID of the "Simple deploy" pipeline and CANARY_CONFIG_ID with the ID of our canary config.
 export SIMPLE_DEPLOY_PIPELINE_ID=$(spin pipelines get -a canary-demo-app -n "Simple deploy" -o jsonpath="{.id}")
 export CANARY_CONFIG_ID=$(spin canary canary-config list -o jsonpath="{[0].id}")
-sed -i "s/SIMPLE_DEPLOY_PIPELINE_ID/$SIMPLE_DEPLOY_PIPELINE_ID/g" ../spinnaker/pipeline-automated-canary.json
+sed -i.orig "s/SIMPLE_DEPLOY_PIPELINE_ID/$SIMPLE_DEPLOY_PIPELINE_ID/g" ../spinnaker/pipeline-automated-canary.json
 sed -i "s/CANARY_CONFIG_ID/$CANARY_CONFIG_ID/g" ../spinnaker/pipeline-automated-canary.json
 
 bold "Create automated canary deploy pipeline"
 spin pipeline save --file ../spinnaker/pipeline-automated-canary.json
-git restore ../spinnaker/pipeline-automated-canary.json
-
+mv ../spinnaker/pipeline-automated-canary.json.orig ../spinnaker/pipeline-automated-canary.json
